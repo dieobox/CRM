c4f3b78258d793bba819634bd65e327be0d55345
Jean <Jean@DESKTOP-H18DUHA>
2019-02-13 16:31:32 +0700

    Jean

---
+++ src/CRM/Controllers/CommentController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using CRM.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using CRM.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CRM.Controllers
+{
+    [Authorize]
+    public class CommentController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private ApplicationDbContext DB;
+        private IHostingEnvironment _environment;
+
+        public CommentController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            RoleManager<IdentityRole> roleManager,
+            ILoggerFactory loggerFactory,
+            ApplicationDbContext db,
+            IHostingEnvironment environment)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            DB = db;
+            _environment = environment;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        
+    }
+}
