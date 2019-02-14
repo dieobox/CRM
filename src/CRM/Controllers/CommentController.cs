be1a499998dd891387a02b584bfcd904e5501b3b
Jean <Jean@DESKTOP-H18DUHA>
2019-02-14 15:19:15 +0700

    Jean

--- src/CRM/Controllers/CommentController.cs
+++ src/CRM/Controllers/CommentController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using CRM.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
+using CRM.ViewModels.Comment;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,6 +24,7 @@ namespace CRM.Controllers
         private readonly RoleManager<IdentityRole> _roleManager;
         private ApplicationDbContext DB;
         private IHostingEnvironment _environment;
+        public CRM.Helpers.Utility Helper;
 
         public CommentController(
             UserManager<ApplicationUser> userManager,
@@ -37,14 +39,68 @@ namespace CRM.Controllers
             _roleManager = roleManager;
             DB = db;
             _environment = environment;
+            Helper = new CRM.Helpers.Utility(_signInManager, DB);
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Index(string CustomerId)
         {
+            ViewBag.CustomerId = CustomerId;
+            ViewBag.CustomerName = DB.Customers.Where(w => w.CustomerId == CustomerId).Select(s => s.CompanyName).FirstOrDefault();
             return View();
         }
-        
+
+        [HttpGet]
+        public IActionResult Gets()
+        {
+            var Gets = DB.Comments;
+            var User = DB.Users;
+            var Customer = DB.Customers;
+            var ViewModels = new List<GetCommentViewModels>();
+            foreach (var Get in Gets)
+            {
+                var Model = new GetCommentViewModels();
+                Model.Title = Get.Title;
+                Model.Comment = Get.Comment;
+                Model.CommentBy = User.Where(w => w.Id == Get.CommentBy).Select(s => s.FirstName + " " + s.LastName).FirstOrDefault();
+                Model.CommentDate = Helper.getShortDateAndTime(Get.CommentDate);
+                Model.CommentId = Get.CommentId;
+                Model.CustomerName = Customer.Where(w => w.CustomerId == Get.CustomerId).Select(s => s.CompanyName).FirstOrDefault();
+                ViewModels.Add(Model);
+            }
+            
+            return PartialView("Gets", ViewModels);
+        }
+
+        [HttpGet]
+        public IActionResult FormAdd(string CustomerId)
+        {
+           
+            ViewBag.CustomerId = CustomerId;
+            return PartialView("FormAdd");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(Comments Model)
+        {
+            var CurrentUser = await _userManager.FindByIdAsync(_userManager.GetUserId(User));
+            string msg = "";
+            try
+            {
+                Model.CommentBy = CurrentUser.Id;
+                Model.CommentDate = DateTime.Now;
+                DB.Comments.Add(Model);
+                DB.SaveChanges();
+                msg = "บันทึกสำเร็จ";
+            }
+            catch (Exception error)
+            {
+                msg = "Error is : " + error.Message;
+                return Json(new { valid = false, message = msg });
+            }
+            return Json(new { valid = true, message = msg });
+        }
         
     }
 }
